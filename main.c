82558a92e442e77b2bd7839da32f8b92ee398479
ItziarDiazHerranz <itziar.diaz-herranz@epitech.eu>
2025-06-12 13:01:11 +0200

    wip: main funcion for workflows

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** chocolatine
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello, Chocolatine!\n");
+    return (0);
+}
